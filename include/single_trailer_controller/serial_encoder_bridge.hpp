4e026f2ec6663d1189562e5b7aa2541bedd2758c
Orlando21Lara <orlando21lara@gmail.com>
2023-05-07 14:36:29 -0400

    Fixed lagging serial communication because the port buffer was holding old data

--- include/single_trailer_controller/serial_encoder_bridge.hpp
+++ include/single_trailer_controller/serial_encoder_bridge.hpp
@@ -12,12 +12,14 @@ private:
     IoContext ctx_;
     drivers::serial_driver::SerialPortConfig config_;
     drivers::serial_driver::SerialPort port_;
+    int initial_encoder_pos_;
 
     rclcpp::TimerBase::SharedPtr timer_;
 
     // Publisher of hitch angle
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr hitch_angle_publisher_;
 
-    // Function that reads serial data and publishes hitch angle
-    void readSerialData();
+    void publishHitchAngle();
+    int readSerialData();   // Function that reads serial data and publishes hitch angle
+    void serialReadCallback(const std::vector<uint8_t> &buffer, const size_t &bytes_transferred);
 };
--- src/serial_encoder_bridge.cpp
+++ src/serial_encoder_bridge.cpp
@@ -1,5 +1,8 @@
 #include "single_trailer_controller/serial_encoder_bridge.hpp"
 
+#define PULSES_PER_REV 600
+#define ENCODER_LEAST_COUNT 360/PULSES_PER_REV
+
 SerialEncoderBridge::SerialEncoderBridge()
     :Node("serial_encoder_bridge_node"),
     config_(115200,
@@ -8,37 +11,68 @@ SerialEncoderBridge::SerialEncoderBridge()
             drivers::serial_driver::StopBits::ONE),
     port_(ctx_, "/dev/ttyUSB0", config_)
 {
-    std::cout << "Serial port device name: " << port_.device_name() << std::endl;
-
-    port_.open();
-    std::cout << "Port is open: " << port_.is_open() << std::endl;
-
+    initial_encoder_pos_ = readSerialData();
 
     // Create publisher for hitch angle
-    hitch_angle_publisher_ = this->create_publisher<std_msgs::msg::Float64>("hitch_angle", 10);
+    hitch_angle_publisher_ = this->create_publisher<std_msgs::msg::Float64>("hitch_angle", 1);
 
     // Initialize and bind the timer callback function to read serial data
-    timer_ = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&SerialEncoderBridge::readSerialData, this));
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(20), std::bind(&SerialEncoderBridge::publishHitchAngle, this));
 }
 
-
-
-void SerialEncoderBridge::readSerialData()
+void SerialEncoderBridge::publishHitchAngle()
 {
-    std::vector<uint8_t> receive_buf(10);
-    int bubus = port_.receive(receive_buf);
+    int current_encoder_pos = readSerialData();
 
-    std::cout << "Printing received bytes: "<< receive_buf.size() << ", bubus:" << bubus << std::endl;
-    for(auto& byte : receive_buf)
-    {
-        std::cout << static_cast<int>(byte);
-    }
-    
-    std::cout << "\n._.\n";
+    // Compute the hitch angle
+    double hitch_angle = (current_encoder_pos - initial_encoder_pos_) * ENCODER_LEAST_COUNT * M_PI / 180.0;
+    // std::cout << "-------------------------------" << std::endl;
+    // std::cout << "Intial encoder pos: " << initial_encoder_pos_ << std::endl;
+    // std::cout << "Current encoder pos: " << current_encoder_pos << std::endl;
+    // std::cout << "Hitch angle (rad): " << hitch_angle << std::endl;
+    // std::cout << "Hitch angle (deg): " << hitch_angle * 180.0 / M_PI << std::endl;
+
     // Publish hitch angle
     std_msgs::msg::Float64 hitch_angle_msg;
-    // hitch_angle_msg.data = std::stod(serial_data);
+    hitch_angle_msg.data = hitch_angle;
     hitch_angle_publisher_->publish(hitch_angle_msg);
+}
+
+int SerialEncoderBridge::readSerialData()
+{
+    port_.open();
+    std::vector<uint8_t> receive_buf(1, 0);
+    std::string received_data = "";
+
+    // Loop until we recieve the new line character
+    while(true)
+    {
+        port_.receive(receive_buf);
+        if(receive_buf[0] == '\n')
+        {
+            break;
+        }
+    }
+
+    // Store the the next characters until we receive a return character
+    while(true)
+    {
+        port_.receive(receive_buf);
+        if(receive_buf[0] == '\r')
+        {
+            break;
+        }
+        received_data += receive_buf[0];
+    }
+    // std::cout << "Received data: " << received_data << std::endl;
+    port_.close();
+
+    return std::stoi(received_data);
+}
+
+void SerialEncoderBridge::serialReadCallback(const std::vector<uint8_t> &buffer, const size_t &bytes_transferred)
+{
+
 }
 
 int main(int argc, char **argv)
